996811fb414c3ddf2de5a79119d5592cfe929b58
thaninpong <thaninpong@perfenterprise.com>
2018-08-25 16:52:09 +0700

    get data
    Signed-off-by: thaninpong <thaninpong@perfenterprise.com>

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { CreateBlockPage } from '../create-block/create-block';
 import { ItemdetailPage } from '../itemdetail/itemdetail';
 import { SlotdetailPage } from '../slotdetail/slotdetail';
@@ -12,43 +12,44 @@ import { HttpClient } from '@angular/common/http';
 export class ContactPage {
   tab1Root = ItemdetailPage;
   tab2Root = SlotdetailPage;
-  Getdata: Getdate;
 
-  constructor(public navCtrl: NavController,private http: HttpClient) {
-    // this.Getdata = this.navParams.data;
+  detaildata: any[];
+
+  constructor(public navCtrl: NavController, private http: HttpClient, public navParams: NavParams) {
+
     this.http.get("http://mrborrowapi.azurewebsites.net/api/Slot/GetItems")
       //  .timeout(10000)
       //  .map(res => res.json())
-       .subscribe((data) => {
-         alert("Data: " + data);
-       },
-         error => {
-           alert("Error: " + error + "\nError message: " + error.message + "\nError result: " + error.error)
-         });
-    
+      .subscribe((data:any) => {
+        this.detaildata = data
+        // console.log(data)
+        // this.detaildata = data
+         console.log(data);
+        // alert("Data: " + data);
+      },
+        error => {
+          alert("Error: " + error + "\nError message: " + error.message + "\nError result: " + error.error)
+        });
+
   }
 
-  
-  Goblock(){
+  Goblock() {
     this.navCtrl.push(CreateBlockPage);
   }
 
-  
-
-  
 }
 
-export class Getdate {
+export class Getdata {
 
   constructor(
-    public id: number,
+    public id: string,
     public name: string,
     public createdDate: string,
     public createdBy: string,
-    public slotId?: string,
-    public slotRow?: string,
-    public slotColumn?: string,
+    public slotId: string,
+    public slotRow: string,
+    public slotColumn: string,
 
-  ) {  }
+  ) { }
 
 }
--- src/pages/create-block/create-block.ts
+++ src/pages/create-block/create-block.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { HttpClient } from '@angular/common/http';
 /**
  * Generated class for the CreateBlockPage page.
  *
@@ -14,12 +14,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'create-block.html',
 })
 export class CreateBlockPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+ item: any = {name: "", row:"" ,column:""};
+  constructor(public navCtrl: NavController, public navParams: NavParams, private httpClient: HttpClient) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateBlockPage');
   }
+
+  createdata(){
+  this.httpClient.post("http://mrborrowapi.azurewebsites.net/api/Slot/CreateItem",
+   this.item,{headers: { 'Content-Type': 'application/json' }}).subscribe((item:any)=>{
+    // alert(JSON.stringify(data));
+    console.log(this.item)
+  },
+  error=>{
+    alert("Error: " + error + "\nError message: " + error.message + "\nError result: " + error.error)
+  })
+
+
+  this.navCtrl.pop();
+  }
+
+  // createdata(){
+
+  //   console.log(this.item);
+  // }
+
 
 }
