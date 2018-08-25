eb4e82cf0879f96050029b2df1dc73c97e4d58a3
sirinarin <sirinarin@perfenterprise.com>
2018-08-25 11:43:31 +0700

    add api project Signed-off-by: sirinarin <sirinarin@perfenterprise.com>

---
+++ api/MrBorrow.Api/Controllers/SlotController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MrBorrow.Api.Models;
+
+namespace MrBorrow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlotController : ControllerBase
+    {
+        private readonly IMongoClient client;
+        private readonly IMongoDatabase database;
+
+        public SlotController()
+        {
+            client = new MongoClient("");
+            database = client.GetDatabase("");
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpGet]
+        public IEnumerable<Slot> GetSlots()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Slot> GetSlot(string slotid)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void CreateSlot([FromBody] Slot slot)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void CreateItemToSlot([FromBody] Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ api/MrBorrow.Api/Models/Slot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MrBorrow.Api.Models
+{
+    public class Slot
+    {
+        public string SlotId { get; set; }
+        public string SlotName { get; set; }
+        public IEnumerable<Item> Items { get; set; }
+        public DateTime? BorrowDate { get; set; }
+        public string Borrower { get; set; }
+        public string Guarantor { get; set; }
+    }
+}
